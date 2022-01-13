a51ffb53813a60af4f97dbc29e8d4ca4d7aadb81
Sjur Moshagen <sjurnm@mac.com>
2022-01-13 09:13:11 +0200

    Kvifor skriv han ikkje 'no'??

--- assets/js/langtable.js
+++ assets/js/langtable.js
@@ -7900,7 +7900,7 @@ function addUnorderedList(repos, mainFilter, filters) {
     }
     // If no repos found, there should be no child elements; tell the user:
     if (ul.childNodes.length === 0) {
-        ul.appendChild(addEmptyLi())
+        // ul.appendChild(addEmptyLi())
         document.write("no");
     } else {
         document.write("yes");
