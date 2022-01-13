1617fae5cb09f9c0b61ef35ba12a4172f13baf91
Sjur Moshagen <sjurnm@mac.com>
2022-01-13 09:26:02 +0200

    Neger test

--- assets/js/langtable.js
+++ assets/js/langtable.js
@@ -7899,7 +7899,7 @@ function addUnorderedList(repos, mainFilter, filters) {
         }
     }
     // If no repos found, there should be no child elements; tell the user:
-    if (ul.childNodes.length > 0) {
+    if (!ul.childNodes.length > 0) {
         // ul.appendChild(addEmptyLi())
         document.write("no");
     } else {
