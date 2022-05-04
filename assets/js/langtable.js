3dc10303a4fa9d1da53a1a2cb6c5f45c278aa8f5
snomos <snomos@users.noreply.github.com>
2022-05-04 20:02:46 +0000

    deploy: 1e8a440877dd0b169283faa4b8591a944a0c787f

--- assets/js/langtable.js
+++ assets/js/langtable.js
@@ -8056,16 +8056,16 @@ function addRepoTable(repos, mainFilter, filters) {
   let heading_4 = document.createElement('th');
   heading_4.innerHTML = 'Issues&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
   let heading_5 = document.createElement('th');
-  heading_5.innerHTML = 'CI&nbsp;Report';
+  heading_5.innerHTML = 'CI&nbsp;Report&nbsp;&nbsp;&nbsp;&nbsp;';
   let heading_6 = document.createElement('th');
-  heading_6.innerHTML = 'Doc&nbsp;build';
+  heading_6.innerHTML = 'Doc&nbsp;build&nbsp;&nbsp;';
 
   row_1.appendChild(heading_1);
   row_1.appendChild(heading_2);
   row_1.appendChild(heading_3);
   row_1.appendChild(heading_4);
-  row_1.appendChild(heading_5);
   row_1.appendChild(heading_6);
+  row_1.appendChild(heading_5);
   thead.appendChild(row_1);
 
   for (const repo of repos) {
@@ -8158,8 +8158,8 @@ function addTR(repo) {
   row.appendChild(row_repo);
   row.appendChild(row_license);
   row.appendChild(row_issues);
-  row.appendChild(row_CI);
   row.appendChild(row_doc);
+  row.appendChild(row_CI);
 
   return row;
 }
