36c93df8e9a33c2e1f3efb2a5bb40f387ca91f16
snomos <snomos@users.noreply.github.com>
2023-03-01 09:29:59 +0000

    deploy: 4b2a25f7a0202716f56dd060c0edb2db2683f1d4

--- assets/js/langtable.js
+++ assets/js/langtable.js
@@ -8049,12 +8049,13 @@ function addRepoTable(repos, mainFilter, filters) {
     let row_1 = document.createElement('tr');
     let heading_1 = document.createElement('th');
     heading_1.innerHTML = 'Documen&shy;tation';
+    heading_1.setAttribute('style', 'width: 25%;');
     let heading_2 = document.createElement('th');
     heading_2.innerHTML = 'Reposi&shy;tory';
     heading_2.setAttribute('style', 'width: 19%;');
     let heading_3 = document.createElement('th');
     heading_3.innerHTML = 'License';
-    heading_3.setAttribute('style', 'width: 15%;');
+    heading_3.setAttribute('style', 'width: 16%;');
     let heading_4 = document.createElement('th');
     heading_4.innerHTML = 'Issues&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
     let heading_5 = document.createElement('th');
