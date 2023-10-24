a588186c5436087d9e25048272392469162f72ae
Sjur N Moshagen <sjurnm@mac.com>
2023-10-24 23:18:26 +0200

    Add special treatment of template repos

--- assets/js/langtable.js
+++ assets/js/langtable.js
@@ -8280,3 +8280,59 @@ function addDictTR(repo) {
 
     return row;
 }
+
+function addTemplateTable(repos, mainFilter, filters) {
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    thead.appendChild(addTableHeader());
+
+    for (const repo of repos) {
+        if (repo.name.startsWith(mainFilter)) {
+            if (filters === null || filters.length === 0) {
+                tbody.appendChild(addTemplTR(repo));
+            } else {
+                if (doesTopicsHaveSomeFilter(repo.topics, filters)) {
+                    tbody.appendChild(addTemplTR(repo));
+                }
+            }
+        }
+    }
+    // If no repos found, inform the user:
+    if (!tbody.firstChild) {
+        tbody.appendChild(addEmptyRow());
+    }
+    return table;
+}
+
+function addTemplTR(repo) {
+    let row = document.createElement('tr');
+
+    let row_lang = document.createElement('td');
+    row_lang.appendChild(addr(reponame2templatename(repo.name), repo.name + '/'));
+
+    row.appendChild(row_lang);
+    row.appendChild(addRepo(repo));
+    row.appendChild(addRLicense(repo));
+    row.appendChild(addIssues(repo));
+    row.appendChild(addRDoc(repo));
+    row.appendChild(addCI(repo));
+
+    return row;
+}
+
+function reponame2templatename(reponame) {
+    parts = reponame.split('-');
+    return code2templatename[parts[2]]
+}
+
+const code2templatename {
+    "corpus":   "Corpora",
+    "dict":     "Dictionaries",
+    "keyboard": "Keyboards",
+    "lang":     "Language models",
+    "shared":   "Shared resources"
+}
