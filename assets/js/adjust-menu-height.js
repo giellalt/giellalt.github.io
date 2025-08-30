97e447e0343247b7a08ac6a7e72fb8a3c3458ae6
snomos <snomos@users.noreply.github.com>
2025-08-30 08:07:01 +0000

    deploy: 06f0bc1820afcecb37154362698a514d1b0deb7b

--- assets/js/adjust-menu-height.js
+++ assets/js/adjust-menu-height.js
@@ -9,13 +9,15 @@ function adjustLeftMenuHeight() {
     availableHeight -= 24; // 1.5em ekstra luft
     leftToc.style.maxHeight = availableHeight + 'px';
     // Skuggeeffekt: berre synleg når menyen kan rullast
+    var shadowDiv = document.getElementById('left_toc_shadow');
     var showShadow = leftToc.scrollHeight > leftToc.clientHeight + 1;
-    if (showShadow) {
-      leftToc.classList.add('show-shadow');
-    } else {
-      leftToc.classList.remove('show-shadow');
+    if (shadowDiv) {
+      if (showShadow) {
+        shadowDiv.classList.add('show-shadow');
+      } else {
+        shadowDiv.classList.remove('show-shadow');
+      }
     }
-    // CSS: .show-shadow::after { opacity: 1; }
   }
 }
 window.addEventListener('resize', adjustLeftMenuHeight);
