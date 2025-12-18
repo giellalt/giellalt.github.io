728aebbe3b8fd51ed07ebda7dc4a6ad862de605d
flammie <flammie@users.noreply.github.com>
2025-12-18 13:10:48 +0000

    deploy: 66d2cb24bddb3752240f9690f583e85e0a2c47d7

--- infra/test-presentation-slidev/assets/slidev/default-BHGJQuGb.js
+++
@@ -1,1 +0,0 @@
-import{f as o,o as t,j as s}from"../modules/vue-DLs7O4Yg.js";import{_ as r}from"../index-8jzP84ZI.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
