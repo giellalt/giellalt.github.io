b7f9139420f622df2db87e596c42511f8f132995
Sjur N Moshagen <sjurnm@mac.com>
2026-04-08 13:04:44 +0300

    Slightly wider table column

--- assets/js/spellertable.js
+++ assets/js/spellertable.js
@@ -95,7 +95,7 @@ function addSpellerTableHeader() {
     heading_4.innerHTML = 'Lemma Count';
     let heading_5 = document.createElement('th');
     heading_5.innerHTML = 'Suggestion Quality';
-    heading_5.setAttribute('style', 'width: 25%;');
+    heading_5.setAttribute('style', 'width: 28%;');
 
     row_1.appendChild(heading_1);
     row_1.appendChild(heading_2);
